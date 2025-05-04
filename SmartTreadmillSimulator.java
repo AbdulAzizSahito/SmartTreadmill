1bd3daab352b247b8ac4b833decff10965916cd8
AbdulAzizSahito <abdulazizsahito238@gmail.com>
2025-05-04 11:39:40 +0500

    Update SmartTreadmillSimulator.java

--- SmartTreadmillSimulator.java
+++ SmartTreadmillSimulator.java
@@ -1,16 +1,21 @@
 // SmartTreadmillSimulator.java
 public class SmartTreadmillSimulator {
-    public static void main(String[] args){
-        System.out.println("=== Smart Treadmill Simulator ===");
-        //aggregation: user passed to treadmill
+    public static void main(String[] args) {
+
+        System.out.println("=== Welcome to Smart Treadmill Simulator ===");
+        
         User user = new User("Ali", 21);
 
-        //Composition: treadmill owns a motor.
+        int warmupDuration = 3;
+        int runningDuration = 5;
+        int cooldownDuration = 2;
+
         Treadmill treadmill = new Treadmill("FitRun", 1500, user);
+        
         try{
-            treadmill.startWorkout();
-        } catch(InterruptedException e){
-            System.out.println("[Error] Workout interrupted.");
+            treadmill.startWorkout(warmupDuration, runningDuration, cooldownDuration);
+        } catch (InterruptedException e){
+            System.out.println("[System] Workout interrupted: " + e.getMessage());
         }
     }
 }
