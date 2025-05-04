45e8b80d58f3a660a747c2ed3a5daf9e8c04c36f
AbdulAzizSahito <abdulazizsahito238@gmail.com>
2025-05-04 11:44:17 +0500

    Create SafetySystem.java

---
+++ SafetySystem.java
@@ -0,0 +1,8 @@
+class SafetySystem{
+    public void checkStatus() {
+        System.out.println("[Safety] Safety key detected. System is secure.");
+    }
+    public void emergencyStop() {
+        System.out.println("[Safety] Emergency stop activated!");
+    }
+}
